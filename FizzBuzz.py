64fa250772d631ec671bc0d254eac63e2e547849
Gonzalo <a20155300@pucp.pe>
2019-03-30 10:48:35 -0500

    Ultima modificacion

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,2 +1,12 @@
+cont = 1
+for i in range(100):
+    if(cont % 3 == 0 and cont % 5 == 0):
+        print("FizzBuzz")
+    elif(cont % 3 == 0):
+        print("Fizz")
+    elif(cont % 5 == 0):
+        print("Buzz")
+    else:
+        print(cont)
 
-for i in range(101)
+    cont += 1
